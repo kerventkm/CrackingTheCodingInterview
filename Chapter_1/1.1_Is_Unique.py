90ecd5d7f386aff538bb48e19504cc33bdeb2722
Kerven Durdymyradov <durdymk@KW-19509.local>
2024-09-02 15:11:55 +0300

    updated

--- Chapter_1/1.1_Is_Unique.py
+++ Chapter_1/1.1_Is_Unique.py
@@ -1,5 +1,24 @@
+
 def IsUniqueBasic(string):
     return len(set(string)) == len(string)
+
+def IsUniqueASCII(string):
+    # Assuming characters are just ASCII
+    
+    if len(string) > 128:
+        return False
+    
+    checker = [None] * 128
+    
+    for char in string:
+        pos = ord(char)
+        
+        if checker[pos]:
+            return False
+        else:
+            checker[pos] = True
+    
+    return True
 
 
 if __name__ == "__main__":
